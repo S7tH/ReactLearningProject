9180243ec0c0ec59d5c089f61fc34109d74a25bf
S7tH <aurelien.theriot@gmail.com>
2021-07-14 16:50:16 +0200

    hook state

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,31 +1,79 @@
-import '../styles/Cart.css'
 import { useState } from 'react'
+import '../styles/Cart.css'
 
 
+export function addToCart(cart, updateCart, name, price) {
+	const currentPlantSaved = cart.find((plant) => plant.name === name)
+	if (currentPlantSaved) {
+		const cartFilteredCurrentPlant = cart.filter(
+			(plant) => plant.name !== name
+		)
+		updateCart([
+			...cartFilteredCurrentPlant,
+			{ name, price, amount: currentPlantSaved.amount + 1 }
+		])
+	} else {
+		updateCart([...cart, { name, price, amount: 1 }])
+	}
+}
 
+function removeToCart(cart, updateCart, name, price) {
 
-function Cart() {
-	const monsteraPrice = 8
-	const [cart, updateCart] = useState(0)
-	const [isOpen, setIsOpen] = useState(false)
+	const currentPlantSToRemove = cart.find((plant) => plant.name === name)
 
+	if (currentPlantSToRemove) {
+		const cartFilteredCurrentPlant = cart.filter(
+			(plant) => plant.name !== name
+		)
 
+		if(currentPlantSToRemove.amount === 1) {
+			updateCart([...cartFilteredCurrentPlant])
+		} else {
+			updateCart([
+				...cartFilteredCurrentPlant,
+				{ name, price, amount: currentPlantSToRemove.amount - 1 }
+			])
+		}
+
+	}
+}
+
+function Cart({ cart, updateCart }) {
+	const [isOpen, setIsOpen] = useState(true)
+	const total = cart.reduce(
+		(acc, plantType) => acc + plantType.amount * plantType.price,
+		0
+	)
 	return isOpen ? (
 		<div className='lmj-cart'>
-			<button onClick={() => setIsOpen(false)}>Fermer</button>
-
-			<h2>Panier</h2>
-			<div>
-				Monstera : {monsteraPrice}€
-				<button onClick={() => updateCart(cart + 1)}>
-					Ajouter
-				</button>
-			</div>
-
-			<h3>Total : {monsteraPrice * cart}€</h3>
-
-			<button onClick={() => updateCart(0)}>Vider le panier</button>
-
+			<button
+				className='lmj-cart-toggle-button'
+				onClick={() => setIsOpen(false)}
+			>
+				Fermer
+			</button>
+			{cart.length > 0 ? (
+				<div>
+					<h2>Panier</h2>
+					<ul>
+						{cart.map(({ name, price, amount }, index) => (
+							<div key={`${name}-${index}`}>
+								<div>
+									{name} {price}€ x {amount}
+								</div>
+								<div>
+								<button onClick={() => addToCart(cart, updateCart, name, price)}>+1</button> / <button onClick={() => removeToCart(cart, updateCart, name, price)}>-1</button>
+								</div>
+								-----------------
+							</div>
+						))}
+					</ul>
+					<h3>Total :{total}€</h3>
+					<button onClick={() => updateCart([])}>Vider le panier</button>
+				</div>
+			) : (
+				<div>Votre panier est vide</div>
+			)}
 		</div>
 	) : (
 		<div className='lmj-cart-closed'>
@@ -37,7 +85,6 @@ function Cart() {
 			</button>
 		</div>
 	)
-
 }
 
 export default Cart
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,8 +1,9 @@
 import { plantList } from '../datas/plantList'
 import PlantItem from './PlantItem'
 import '../styles/ShoppingList.css'
+import { addToCart } from '../components/Cart'
 
-function ShoppingList() {
+function ShoppingList({ cart, updateCart }) {
 	const categories = plantList.reduce(
 		(acc, plant) =>
 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
@@ -10,21 +11,24 @@ function ShoppingList() {
 	)
 
 	return (
-		<div>
+		<div className='lmj-shopping-list'>
 			<ul>
 				{categories.map((cat) => (
 					<li key={cat}>{cat}</li>
 				))}
 			</ul>
 			<ul className='lmj-plant-list'>
-				{plantList.map(({ id, cover, name, water, light }) => (
-					<PlantItem
-						id={id}
-						cover={cover}
-						name={name}
-						water={water}
-						light={light}
-					/>
+				{plantList.map(({ id, cover, name, water, light, price }) => (
+					<div key={id}>
+						<PlantItem
+							cover={cover}
+							name={name}
+							water={water}
+							light={light}
+							price={price}
+						/>
+						<button onClick={() => addToCart(cart, updateCart, name, price)}>Ajouter</button>
+					</div>
 				))}
 			</ul>
 		</div>
